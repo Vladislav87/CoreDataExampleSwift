9bb7617092d5f2254ead757dcaadac9f4a8fdfe1
Vlad Samoilov <vlad_pro@icloud.com>
2016-12-18 14:39:38 +0300

    Initial Commit

---
+++ FoodList_v_2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FoodList_v_2
+//
+//  Created by Владислав Ходеев on 18.12.16.
+//  Copyright © 2016 Vlad Samoilov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
